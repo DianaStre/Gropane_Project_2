cf2e8e8428913be4a7e946961de15c5b73d847b8
AID312 <aidan.osman@yahoo.com>
2024-11-04 23:51:34 +0200

    update module_aidan
    update module_aidan

--- module_aidan.py
+++ module_aidan.py
@@ -2,22 +2,14 @@ from module_george import generate_road
 from module_vlad import play_stage
 
 def play_game():
-    """
-    Aidan: Manages the overall game flow, transitioning through stages from start to end.
-    """
-    starting_point, ending_point, stages = generate_road()
-    print ("This is the story of an unfortunate man that left with his car from the best mechanic in the city, and his life depends on his luck on the way home.")
-
-    print ("Hope you will be a lucky man because you're car wasn't that lucky")
-    print(f"Starting your journey from {starting_point} to {ending_point}.\n")
-
-    for i, stage in enumerate(stages):
+    starting_point, ending_point, stage = generate_road()
+    print(f"You start from {starting_point} to {ending_point}.")
+    for i, stage in enumerate(stage):
         stage_name = ["vest", "centru", "est"][i]
-        print(f"--- Entering {stage_name.capitalize()} Romania ---")
+        print(f" You are in {stage_name} city ")
 
-        # Play the current stage
+        # play current city
         if not play_stage(stage, stage_name):
-            return  # End game if the player loses all HP in any stage
-
-    # If all stages are completed, the player wins
-    print("Congratulations! You have successfully reached Iasi and completed the game!")
+            return  # game over  if the player loses his life in a stage
+    # the player wins If he arrives in iasi
+    print("You arrived in Iasi!")
