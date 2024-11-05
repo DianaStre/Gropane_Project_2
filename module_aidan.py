bc23c96867e3f24f0cd4b569261b0bc6aea49007
Melnicki13 <130988592+Melnicki13@users.noreply.github.com>
2024-11-05 13:01:16 +0200

    Ascii_and_presentation
    Added the title with ascii and a little background story

--- module_aidan.py
+++ module_aidan.py
@@ -3,6 +3,18 @@ from module_vlad import play_stage
 
 def play_game():
     starting_point, ending_point, stage = generate_road()
+    print("""
+      _____ _            _       _                 _                        
+     |_   _| |__   ___  / \   __| |_   _____ _ __ | |_ _   _ _ __ ___ _ __  
+       | | | '_ \ / _ \/ _ \ / _` \ \ / / _ \ '_ \| __| | | | '__/ _ \ '__| 
+       | | | | | |  __/ ___ \ (_| |\ V /  __/ | | | |_| |_| | | |  __/ |    
+       |_| |_| |_|\___/_/   \_\__,_| \_/ \___|_| |_|\__|\__,_|_|  \___|_|    
+
+        """)
+    print("This is the story of an unfortunate man who went to the best "
+          "mechanic in the village, after he left the mechanic his way "
+          "to home depended more on Pure Luck:D")
+    print("SO LET THE ADVENTURE BEGIN !")
     print(f"You start from {starting_point} to {ending_point}.")
     for i, stage in enumerate(stage):
         stage_name = ["vest", "centru", "est"][i]
