0bd5c7f69ecf82af5fd99e66936e94e9ce124ab2
DianaStreinu <dookie_ado@yahoo.com>
2024-11-06 12:55:15 +0200

    added colorful opening statement

--- module_aidan.py
+++ module_aidan.py
@@ -14,8 +14,8 @@ def play_game():
     print("This is the story of an unfortunate man who went to the best "
           "mechanic in the village, after he left the mechanic his way "
           "to home depended more on Pure Luck:D")
-    print("SO LET THE ADVENTURE BEGIN !")
-    print(f"You start from {starting_point} to {ending_point}.")
+    print("\033[92mSO LET THE ADVENTURE BEGIN !\033[0m")
+    print(f"You start from {starting_point} and you want to go home to {ending_point}.")
     for i, stage in enumerate(stage):
         stage_name = ["western", "central", "eastern"][i]
         print(f"\033[93mYou are in the \033[91m{stage_name}\033[93m side of the country\033[0m")
