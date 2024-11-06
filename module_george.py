9c7cfe89fdd7c38449bb140363271ab346fc6eec
gxyvyxg <george.v.ghidiu@gmail.com>
2024-11-06 20:57:14 +0100

    Update module_george.py

--- module_george.py
+++ module_george.py
@@ -4,8 +4,6 @@ from random import random, randint
 def generate_road():
     """
     George: Defines the game map with cities and damage values for each region of Romania.
-    The inside joke here is that the western part of the country has better roads, but as you progress east,
-    the roads get worse and worse.
     """
     # player starts in Timisoara, having just repaired their car
     # player wants to go home to Iasi
@@ -23,45 +21,54 @@ def generate_road():
     ending_point = "Iasi"
 
     harta_vest_romania = {
-        "Arad": randint(0,1),
-        "Oradea": randint(0,1),
-        "Satu Mare": -1,
-        "Sighetu Marmatiei": randint(0,1),
-        "Craiova": randint(0,1),
-        "Zalau": -1,
-        "Deva": randint(0,1),
-        "Drobeta-Turnu Severin": randint(0,1),
-        "Baia Mare": -1,
-        "Caransebes": randint(0,1)
+        "Arad": random.choice([-1, 0, 1]),
+        "Oradea": random.choice([-1, 0, 1]),
+        "Satu Mare": 1,
+        "Sighetu Marmatiei": random.choice([-1, 0, 1]),
+        "Craiova": random.choice([-1, 0, 1]),
+        "Zalau": 1,
+        "Deva": random.choice([-1, 0, 1]),
+        "Drobeta-Turnu Severin": random.choice([-1, 0, 1]),
+        "Baia Mare": 1,
+        "Caransebes": random.choice([-1, 0, 1]),
+        "Lugoj":-5,
+        "Resita":1
     }
 
     harta_centru_romania = {
-        "Cluj-Napoca": 0,
+        "Cluj-Napoca": random.choice([-1, 0, 1]),
         "Targu Mures": 0,
-        "Ramnicu Valcea": -1,
-        "Bucuresti": 0,
-        "Brasov": 0,
-        "Miercurea Ciuc": -1,
-        "Targoviste": 0,
-        "Turda": 0,
-        "Pitesti": -1,
-        "Ploiesti": 0
+        "Ramnicu Valcea": random.choice([-1, 0, 1]),
+        "Bucuresti": random.choice([-1, 0, 1]),
+        "Brasov": random.choice([-1, 0, 1]),
+        "Miercurea Ciuc": 0,
+        "Targoviste": random.choice([-1, 0, 1]),
+        "Turda": random.choice([-1, 0, 1]),
+        "Pitesti": random.choice([-1, 0, 1]),
+        "Ploiesti": 0,
+        "Orastie": -5,
+        "Dej": -5,
+        "Bistrita": 0,
+        "Sighiisoara": random.choice([-1, 0, 1])
     }
 
     harta_est_romania = {
-        "Constanta": -1,
-        "Braila": -1,
-        "Galati": 0,
+        "Constanta": random.choice([-1, 0, 1]),
+        "Braila": random.choice([-1, 0, 1]),
+        "Galati": random.choice([-1, 0, 1]),
         "Focsani": -1,
         "Botosani": -1,
-        "Suceava": 0,
+        "Suceava": random.choice([-1, 0, 1]),
         "Piatra Neamt": -1,
         "Bacau": -1,
-        "Ramnicu Sarat": 0,
-        "Vaslui": 1,
-        "Tecuci": 0,  # New city with 0 damage
-        "Barlad": 0,  # New city with +1 HP
-        "Roman": 1  # New city with +1 HP
+        "Ramnicu Sarat": random.choice([-1, 0, 1]),
+        "Vaslui": random.choice([-1, 0, 1]),
+        "Tecuci": -5,
+        "Barlad": -5,
+        "Roman": -5,
+        "Husi": random.choice([-1, 0, 1]),
+        "Pascani": random.choice([-1, 0, 1]),
+        "Gura Humorului": random.choice([-1, 0, 1])
     }
 
     return starting_point, ending_point, [harta_vest_romania, harta_centru_romania, harta_est_romania]
