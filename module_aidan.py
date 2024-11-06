8d6919bb891eee11abe66bc260111bc54331817e
DianaStreinu <dookie_ado@yahoo.com>
2024-11-06 09:54:20 +0200

    some changes

--- module_aidan.py
+++ module_aidan.py
@@ -18,7 +18,7 @@ def play_game():
     print(f"You start from {starting_point} to {ending_point}.")
     for i, stage in enumerate(stage):
         stage_name = ["west", "center", "east"][i]
-        print(f" You are in the {stage_name} side of the country ")
+        print(f" You are in the {stage_name} side of the country")
 
         # play current city
         if not play_stage(stage, stage_name):
